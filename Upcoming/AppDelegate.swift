13af37520dc86dcaf51d6744c62a54600be03fc6
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2026-02-15 16:03:44 -0500

    Xcode new project

---
+++ Upcoming/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Upcoming
+//
+//  Created by Travis Luckenbaugh on 2/15/26.
+//
+
+import UIKit
+
+@main class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ Upcoming/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Upcoming
+//
+//  Created by Travis Luckenbaugh on 2/15/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
